a07adc305dcc23ad0b7e3d775cb32fd22cdb5a8d
DESKTOP-SQ19H3F\Lilin <1436316404@qq.com>
2022-07-29 09:06:36 +0800

    提交一轮测试代码

---
+++ testTeamCity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testTeamCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输出一条helloworld");
+
+            Console.ReadKey();
+        }
+    }
+}
